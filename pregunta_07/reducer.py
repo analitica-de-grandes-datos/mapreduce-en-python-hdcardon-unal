176e4662c8f10a8ed1162738485941bc9bce9652
hdcardon <65559919+hdcardon-unal@users.noreply.github.com>
2022-07-04 15:50:58 -0500

    Commit #6 Algoritmo MapReduce en Python
    Desarrollo LAB MapReduce en Python

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    word = []
+    date = []
+    num = []
+
+    for line in sys.stdin:
+        word.append(line.split("\t")[0])
+        date.append(line.split("\t")[1])
+        num.append(int(line.split("\t")[2]))
+
+    ky_value = zip(word, date, num)
+
+    ky_value_2 = sorted(ky_value,key=lambda x:(x[0],x[2]))
+
+    for word, date, num in ky_value_2:
+      sys.stdout.write("{}   {}   {}\n".format(word, date, num))
